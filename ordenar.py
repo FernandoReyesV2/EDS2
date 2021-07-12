a86332e4ce75f66a8db12de7393dd218f890f9a1
FernandoReyesV2 <85742370+FernandoReyesV2@users.noreply.github.com>
2021-07-12 12:23:31 -0500

    Add files via upload

--- ordenar.py
+++ ordenar.py
@@ -32,8 +32,67 @@ class Ordernar:
                     self.lista[pos]=self.lista[sig]
                     self.lista[sig]=aux
 
-lista= [2,3,1,5,8,10]
-ord1=Ordernar(lista)
+    def ordernarDes(self):
+        for pos,ele in enumerate(self.lista):
+            for sig in range(pos+1,len(self.lista)):
+                if ele < self.lista[sig]:
+                    aux=self.lista[pos]
+                    self.lista[pos]=self.lista[sig]
+                    self.lista[sig]=aux
+
+    def primer(self):
+        return self.lista[0]
+
+    def primerEliminado(self):
+        primer=self.lista[0]
+        auxlista=[]
+        for pos in range(1,len(self.lista)):
+            auxlista.append(self.lista[pos])
+        self.lista=auxlista
+        return primer
+
+    def primerEliminado2(self):
+        primer = self.lista[0]
+        self.lista=self.lista[1:]
+        return primer
+
+    def ultimo(self):
+        return self.lista[-1]
+
+    def insertar(self,num):
+        self.ordenarAs()
+        auxlista=[]
+        for pos, ele in enumerate(numeros):
+            if num < ele:
+                break
+        self.lista=self.lista[0:pos]+auxlista+self.lista[pos:]
+
+    def insertar2(self,num):
+        self.ordenarAs()
+        auxlista=[]
+        for pos, ele in enumerate(self.lista):
+            if num < ele:
+                break
+        for i in range(pos):
+            auxlista.append(self.lista[i]):
+        auxlista.append(num)
+        for j in range(pos, len((self.lista))):
+            auxlista.append(self.lista[j])
+        self.lista + auxlista
+
+    def insetarOrden(self,num):
+        self.lista.append(num)
+        self.ordenarAs()
+
+    def eliminar(self,num):
+        enc=False
+    for pos, ele in enumerate(self.lista):
+        if num == ele:
+            enc=True
+            break
+    if enc: self.lista=self.lista[0:pos]+self.lista[pos+1:]
+    return enc
+
 #ord1.recorrerElemento()
 #ord1.recorrerEnumarate()
 #ord1.recorrerRange()
@@ -45,6 +104,18 @@ ord1=Ordernar(lista)
 #   print("El numero {} se encuentra en la posicion ({}) de la lista{}".format(buscado,resp,ord1.lista))
 #else:
 #    print("El numero {} no se encuentra en la lista {}".format(buscado,ord1.lista))
-print(ord1.lista)
-ord1.ordenarAs()
-print(ord1.lista)
+#print("Primer",ord1.primer())
+#print("Ultimo",ord1.ultimo())
+#print("Normal", ord1.lista)
+#ord1.ordenarAs()
+#print("Ascendente",ord1.lista)
+#ord1.ordernarDes()
+#print("Descendente",ord1.lista)
+lista= [2,3,1,5,8,10]
+# ord1=Ordernar(lista)
+# print(ord1.lista)
+# print("Primer Eliminado",ord1.primerEliminado())
+# print(ord1.lista)
+ord1=Ordernar(lista)
+if ord1.eliminar(8)==True: print("El numero se elimino de la lista",ord1.lista)
+else: print("El numero no se encuentra en la lista")
